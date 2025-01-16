261a2145cb3095a1d382402bd25e113fcbe01860
Luffo-lol <139163127+Luffo-lol@users.noreply.github.com>
2025-01-16 16:22:59 +0000

    fixed code

--- C++/main.cpp
+++ C++/main.cpp
@@ -32,11 +32,10 @@ class Problem {
     }
 
     int readFile (string filename){
-        std::ifstream file(filename);
+        std::ifstream file("readFiles/" + filename);
         vector<long double> constants; //Scheme, T, dt, g, rho, Cd
 
         if (!file) {
-            cout << "Error opening file!" << endl;
             return 1;
         }
 
@@ -51,7 +50,7 @@ class Problem {
 
         for (char c : line) {
             if (c != ' ') {
-                temp += c;  // Add character to temp if it's not a space
+                temp += c;  // Add character to temp if it's not a space'
             } 
             else {
                 if (!temp.empty()) {
@@ -62,6 +61,7 @@ class Problem {
         }
         if (!temp.empty()){
             constants.push_back(stod(temp));  // Converts final element
+            temp = "";
         }
 
 
@@ -82,7 +82,7 @@ class Problem {
                     } 
                     else {
                         if (!temp.empty()) {
-                            row.push_back(stod(temp));  // Convert temp to an integer and add it to the vector
+                            row.push_back(stod(temp));  // Convert temp to a double and add it to the vector
                             temp = "";  // Reset temp for the next number
                         }
                     }
@@ -98,11 +98,11 @@ class Problem {
         return 0;
     }      
 
-    int finddh (long double m, long double A, long double K, long double yt, long double ht){
+    long double finddh (long double m, long double A, long double K, long double yt, long double ht){
         return  (K/(rho*A) * (m/(rho*A) + yt - ht));
     }
 
-    int findAcc (long double m, long double A, long double K, long double yt, long double ht, long double vt){
+    long double findAcc (long double m, long double A, long double K, long double yt, long double ht, long double vt){
         return ((rho*A)/(m + rho*A*ht) * (2*g*ht - g*yt - 0.5*Cd*abs(vt)*vt));
     }
 
@@ -127,27 +127,36 @@ class Problem {
         vector<long double> row = {0, y0, v0, h0};
         out.push_back(row);
         for(long double t = dt; t <= T; t += dt){
-            long double yk1 = row[2]*dt;
             long double vk1 = findAcc(m,A,K,row[1],row[2],row[3])*dt;
+            long double yk1 = row[2]*dt;
             long double hk1 = finddh(m,A,K,row[1],row[3])*dt;
             
             long double vk2 = dt * findAcc(m,A,K,row[1] + yk1/2, row[2] + vk1/2, row[3] + hk1/2);
-            long double yk2 = row[2] + (dt/2) * findAcc(m,A,K,row[1] + yk1/2, row[2] + vk1/2, row[3] + hk1/2);
+            long double yk2 = dt * (row[2] + findAcc(m,A,K,row[1] + yk1/2, row[2] + vk1/2, row[3] + hk1/2));
             long double hk2 = dt * finddh(m,A,K,row[1] + yk1/2,row[3] + hk1/2);
 
             long double vk3 = dt * findAcc(m,A,K,row[1] + yk2/2, row[2] + vk2/2, row[3] + hk2/2);
-            long double yk3 = row[2] + (dt/2) * findAcc(m,A,K,row[1] + yk2/2, row[2] + vk2/2, row[3] + hk2/2);
+            long double yk3 = dt * (row[2] + findAcc(m,A,K,row[1] + yk2/2, row[2] + vk2/2, row[3] + hk2/2));
             long double hk3 = dt * finddh(m,A,K,row[1] + yk2/2,row[3] + hk2/2);
 
             long double vk4 = dt * findAcc(m,A,K,row[1] + yk3, row[2] + vk3, row[3] + hk3);
-            long double yk4 = row[2] + dt * findAcc(m,A,K,row[1] + yk3, row[2] + vk3, row[3] + hk3);
+            long double yk4 = dt * (row[2] + findAcc(m,A,K,row[1] + yk3, row[2] + vk3, row[3] + hk3));
             long double hk4 = dt * finddh(m,A,K,row[1] + yk3,row[3] + hk3);
+            
+            //cout << "vk" << endl;
+            //cout << vk1 << ", " << vk2 << ", "<< vk3 << ", " << vk4 << endl;
+            //cout << "yk" << endl;
+            //cout << yk1 << ", " << yk2 << ", "<< yk3 << ", " << yk4 << endl;
+            //cout << "hk" << endl;
+            //cout << hk1 << ", " << hk2 << ", "<< hk3 << ", " << hk4 << endl;
 
-            long double yn = row[1] + 1/6 * (yk1 + 2*yk2 + 2*yk3 + yk4);
-            long double vn = row[2] + 1/6 * (vk1 + 2*vk2 + 2*vk3 + vk4);
-            long double hn = row[3] + 1/6 * (hk1 + 2*hk2 + 2*hk3 + hk4);
+            long double yn = row[1] + 1.0/6.0 * (yk1 + 2*yk2 + 2*yk3 + yk4);
+            long double vn = row[2] + 1.0/6.0 * (vk1 + 2*vk2 + 2*vk3 + vk4);
+            long double hn = row[3] + 1.0/6.0 * (hk1 + 2*hk2 + 2*hk3 + hk4);
             row = {t, yn, vn, hn};
-            //cout << "t = " << t << "y = " << yn << "v = " << vn << "hn = " << hn << endl;
+
+            //cout << "t = " << t << " y = " << yn << " v = " << vn << " hn = " << hn << endl;
+            
             out.push_back(row);
         }
         cout << "Returned" << endl;
@@ -158,7 +167,7 @@ class Problem {
     void generateMatrix(){//generates 3D matrix of the motion of the time, position, velocity, height for each time step for each of the vessel parameters
         
         for (int vesselNumber = 0; vesselNumber < data.size();vesselNumber++){//for each vessel being evaluated 
-            cout << "Currently Porcessing VesselNumber: " << vesselNumber << endl;
+            cout << "Currently Porcessing VesselNumber: " << vesselNumber + 1 << endl;
             cout << "m = " << data[vesselNumber][0] << " A = " <<  data[vesselNumber][1] << " K = " <<  data[vesselNumber][2] << " y0 = " << data[vesselNumber][3] << " v0 = " << data[vesselNumber][4] << " h0 = " << data[vesselNumber][5] << endl;
             if(scheme == 0){
                 evaluatedMatrix.push_back(FE(data[vesselNumber][0], data[vesselNumber][1], data[vesselNumber][2], data[vesselNumber][3], data[vesselNumber][4], data[vesselNumber][5]));
@@ -171,8 +180,20 @@ class Problem {
 
     void saveToFile(){
         for (int vesselNumber = 0; vesselNumber < data.size();vesselNumber++){
-            string outFileName = "positionData-" + inFileName + "-" + to_string(vesselNumber) + ".txt";
+            string schemeName = "";
+            if (scheme == 0){
+                schemeName = "FE";
+            }
+            if (scheme == 1){
+                schemeName = "RK4";
+            }
+
+            string outFileName = "writeFiles/positionData-" + inFileName + "-" + to_string(vesselNumber + 1) + "-" + schemeName + ".txt";
             ofstream outFile(outFileName);
+            outFile << inFileName << " - Vessel Number " << to_string(vesselNumber + 1) << endl;
+            outFile << "m = " << data[vesselNumber][0] << " A = " <<  data[vesselNumber][1] << " K = " <<  data[vesselNumber][2] << " y0 = " << data[vesselNumber][3] << " v0 = " << data[vesselNumber][4] << " h0 = " << data[vesselNumber][5] << endl;
+            outFile << "t " << "yn " << "vn " << "hn" << endl; 
+
             cout << outFileName << endl;
             if (outFile.is_open()) {
                 for(int rowNumber = 0; rowNumber < size(evaluatedMatrix[vesselNumber]); rowNumber++){
@@ -194,4 +215,8 @@ class Problem {
 
 int main() {
     Problem firstTest = Problem("parameters.txt");
+    Problem Q3Part1 = Problem("SV-ND.txt");
+    Problem Q3Part2 = Problem("SV-LD.txt");
+    Problem Q3Part3 = Problem("SinkingVessel.txt");
+    Problem Q3Part4 = Problem("ThreeVessels.txt");
 }
